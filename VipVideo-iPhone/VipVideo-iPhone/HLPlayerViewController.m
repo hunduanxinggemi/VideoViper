690c73ee87eb89466dfec48e2f72459186458168
Hunduanxinggemi <haoran223@163.com>
2020-12-15 10:38:57 +0800

    init

--- VipVideo-iPhone/VipVideo-iPhone/HLPlayerViewController.m
+++ VipVideo-iPhone/VipVideo-iPhone/HLPlayerViewController.m
@@ -4,35 +4,36 @@
 //
 //  Created by LHL on 2018/1/23.
 //  Copyright © 2018年 SV. All rights reserved.
-//
+//  播放器的控制器
 
 #import "HLPlayerViewController.h"
-#import "WMPlayer.h"
-#import "QSPDownloadTool.h"
 #import "HTTPServer.h"
+#import "QSPDownloadTool.h"
+#import "WMPlayer.h"
 
-@interface HLPlayerViewController ()<WMPlayerDelegate>
+@interface HLPlayerViewController () <WMPlayerDelegate>
 
-@property (nonatomic, strong) WMPlayer *wmPlayer;
+@property (nonatomic, strong) WMPlayer *  wmPlayer;
 @property (nonatomic, strong) HTTPServer *server;
 
 @end
 
 @implementation HLPlayerViewController
 
-- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
         self.canDownload = YES;
     }
     return self;
 }
 
-- (void)dealloc{
+- (void)dealloc {
     [self.server stop];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void)viewWillDisappear:(BOOL)animated{
+- (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     [self.wmPlayer pause];
 }
@@ -42,49 +43,51 @@
 
     //获取设备旋转方向的通知,即使关闭了自动旋转,一样可以监测到设备的旋转方向
     [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-//    //旋转屏幕通知
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(onDeviceOrientationChange:)
-                                                 name:UIDeviceOrientationDidChangeNotification
-                                               object:nil];
-    
-    
-    WMPlayerModel *playerModel = [[WMPlayerModel alloc]init];
-    playerModel.title = self.title?:@"播放中";
-    
+    //    //旋转屏幕通知
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(onDeviceOrientationChange:)
+               name:UIDeviceOrientationDidChangeNotification
+             object:nil];
+
+    WMPlayerModel *playerModel = [[WMPlayerModel alloc] init];
+    playerModel.title          = self.title ?: @"播放中";
+
     if ([self.url.absoluteString hasPrefix:@"http"]) {
         playerModel.videoURL = self.url;
-    }else {
-        
-        if(!self.server){
+    } else {
+        if (!self.server) {
             self.server = [[HTTPServer alloc] init];
             [self.server setType:@"_http.tcp"];
             [self.server setPort:8890];
-            
         }
-        
-//        if ([self.server isRunning]) {
-//            [self.server stop];
-//        }
+        //
+        if ([self.server isRunning]) {
+            [self.server stop];
+        }
         //设置服务器根路径
-        NSString *newurl = [self.url.absoluteString.stringByDeletingLastPathComponent stringByReplacingOccurrencesOfString:@"file:" withString:@""];
+        NSString *newurl =
+            [self.url.absoluteString.stringByDeletingLastPathComponent
+                stringByReplacingOccurrencesOfString:@"file:"
+                                          withString:@""];
         [self.server setDocumentRoot:newurl];
 
-        
-        if(![self.server isRunning]){
+        if (![self.server isRunning]) {
             NSError *error = nil;
             [self.server start:&error];
-            NSLog(@"xxx %@",error);
-            NSLog(@"yyy %@",self.server.documentRoot);
+            NSLog(@"xxx %@", error);
+            NSLog(@"yyy %@", self.server.documentRoot);
         }
-        
+
         NSLog(@"是否启动 %@", @([self.server isRunning]));
-        
-        playerModel.videoURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://localhost:8890/index.m3u8"]];
+
+        playerModel.videoURL = [NSURL
+            URLWithString:[NSString stringWithFormat:
+                                        @"http://localhost:8890/index.m3u8"]];
     }
 
-
-    self.wmPlayer = [WMPlayer playerWithModel:playerModel];;
+    self.wmPlayer = [WMPlayer playerWithModel:playerModel];
+    ;
     self.wmPlayer.canDownload = self.canDownload;
     [self.view addSubview:self.wmPlayer];
     [self.wmPlayer mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -93,88 +96,120 @@
 
     self.wmPlayer.delegate = self;
     [self.wmPlayer play];
-    
+
     NSLog(@"current url %@", self.url);
 }
 
 - (void)forceInterfaceOrientation:(UIInterfaceOrientation)orientation {
-    if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
-        SEL selector             = NSSelectorFromString(@"setOrientation:");
-        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
+    if ([[UIDevice currentDevice]
+            respondsToSelector:@selector(setOrientation:)]) {
+        SEL           selector   = NSSelectorFromString(@"setOrientation:");
+        NSInvocation *invocation = [NSInvocation
+            invocationWithMethodSignature:
+                [UIDevice instanceMethodSignatureForSelector:selector]];
         [invocation setSelector:selector];
         [invocation setTarget:[UIDevice currentDevice]];
-        int val                  = orientation;
+        int val = orientation;
         [invocation setArgument:&val atIndex:2];
         [invocation invoke];
     }
 }
 
-
-- (void)setUrl:(NSURL *)url{
+- (void)setUrl:(NSURL *)url {
     _url = url;
 }
 
-- (void)reloadRequest{
-    WMPlayerModel *playerModel = [[WMPlayerModel alloc]init];
-    playerModel.title = self.title?:@"播放中";
-    playerModel.videoURL = self.url;
-    self.wmPlayer.playerModel = playerModel;
+- (void)reloadRequest {
+    WMPlayerModel *playerModel = [[WMPlayerModel alloc] init];
+    playerModel.title          = self.title ?: @"播放中";
+    playerModel.videoURL       = self.url;
+    self.wmPlayer.playerModel  = playerModel;
     [self.wmPlayer play];
 }
 
 /** backBtn event */
-- (void)zf_playerBackAction{
+- (void)zf_playerBackAction {
     if (self.backCompleteBlock) {
         self.backCompleteBlock(YES);
     }
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)onDeviceOrientationChange:(NSNotification *)notification{
-    
-    NSLog(@"屏幕需要旋转");
+- (void)onDeviceOrientationChange:(NSNotification *)notification {
+    //添加是否需要旋转的功能
+    NSLog(@"目前播放器是否是锁定的状态 %d", self.wmPlayer.isLockScreen);
+    if (self.wmPlayer.isLockScreen) {
+        return;
+    }
 }
 
 /** downloadBtn event */
-- (void)zf_playerDownload:(NSString *)url{
+- (void)zf_playerDownload:(NSString *)url {
     if (url) {
-        [[QSPDownloadTool shareInstance] addDownloadToolDelegate:(id)self];
-        [[QSPDownloadTool shareInstance] addDownloadTast:url title:self.title?:self.navigationItem.title andOffLine:YES];
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"已添加到下载任务" message:nil preferredStyle:UIAlertControllerStyleAlert];
-        UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+        [[QSPDownloadTool shareInstance] addDownloadToolDelegate:(id) self];
+        [[QSPDownloadTool shareInstance]
+            addDownloadTast:url
+                      title:self.title ?: self.navigationItem.title
+                 andOffLine:YES];
+        UIAlertController *alertController = [UIAlertController
+            alertControllerWithTitle:@"已添加到下载任务"
+                             message:nil
+                      preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action =
+            [UIAlertAction actionWithTitle:@"确定"
+                                     style:UIAlertActionStyleDefault
+                                   handler:nil];
         [alertController addAction:action];
-        [self presentViewController:alertController animated:YES completion:nil];
+        [self presentViewController:alertController
+                           animated:YES
+                         completion:nil];
     }
 }
 
-- (void)downloadSource:(QSPDownloadSource *)source changedStyle:(QSPDownloadSourceStyle)style{
+- (void)downloadSource:(QSPDownloadSource *)source
+          changedStyle:(QSPDownloadSourceStyle)style {
     if (style == QSPDownloadSourceStyleFail) {
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"下载失败" message:nil preferredStyle:UIAlertControllerStyleAlert];
-        UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+        UIAlertController *alertController = [UIAlertController
+            alertControllerWithTitle:@"下载失败"
+                             message:nil
+                      preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action =
+            [UIAlertAction actionWithTitle:@"确定"
+                                     style:UIAlertActionStyleDefault
+                                   handler:nil];
         [alertController addAction:action];
-        [self presentViewController:alertController animated:YES completion:nil];
+        [self presentViewController:alertController
+                           animated:YES
+                         completion:nil];
     }
-} 
+}
 
-- (void)downloadToolDidFinish:(QSPDownloadTool *)tool downloadSource:(QSPDownloadSource *)source{
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"下载成功" message:nil preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+- (void)downloadToolDidFinish:(QSPDownloadTool *)tool
+               downloadSource:(QSPDownloadSource *)source {
+    UIAlertController *alertController = [UIAlertController
+        alertControllerWithTitle:@"下载成功"
+                         message:nil
+                  preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action =
+        [UIAlertAction actionWithTitle:@"确定"
+                                 style:UIAlertActionStyleDefault
+                               handler:nil];
     [alertController addAction:action];
     [self presentViewController:alertController animated:YES completion:nil];
 }
 
-
 /** 控制层即将显示 */
-- (void)zf_playerControlViewWillShow:(UIView *)controlView isFullscreen:(BOOL)fullscreen{
+- (void)zf_playerControlViewWillShow:(UIView *)controlView
+                        isFullscreen:(BOOL)fullscreen {
     NSLog(@"");
 }
 
 /** 控制层即将隐藏 */
-- (void)zf_playerControlViewWillHidden:(UIView *)controlView isFullscreen:(BOOL)fullscreen{
-    
+- (void)zf_playerControlViewWillHidden:(UIView *)controlView
+                          isFullscreen:(BOOL)fullscreen {
 }
 
-- (BOOL)shouldAutorotate{
+- (BOOL)shouldAutorotate {
     return YES;
 }
 
@@ -190,17 +225,17 @@
 
 #pragma mark - WMPlayer Delegate
 
--(void)wmplayer:(WMPlayer *)wmplayer clickedFullScreenButton:(UIButton *)fullScreenBtn{
+- (void)wmplayer:(WMPlayer *)wmplayer
+    clickedFullScreenButton:(UIButton *)fullScreenBtn {
     if (!wmplayer.isFullscreen) {
         [self forceInterfaceOrientation:UIInterfaceOrientationLandscapeRight];
-    }else {
+    } else {
         [self forceInterfaceOrientation:UIInterfaceOrientationPortrait];
     }
     wmplayer.isFullscreen = !wmplayer.isFullscreen;
 }
 
-
--(void)wmplayer:(WMPlayer *)wmplayer clickedCloseButton:(UIButton *)backBtn{
+- (void)wmplayer:(WMPlayer *)wmplayer clickedCloseButton:(UIButton *)backBtn {
     if (self.backCompleteBlock) {
         self.backCompleteBlock(YES);
     }
@@ -209,13 +244,13 @@
     }
 }
 
-- (void)wmplayer:(WMPlayer *)wmplayer clickedDownloadButton:(UIButton *)backBtn{
+- (void)wmplayer:(WMPlayer *)wmplayer
+    clickedDownloadButton:(UIButton *)backBtn {
+#pragma mark 需要在这里进行存储需要下载的videoUrl数组。然后在在下载完成后删除该URl 并进行排重操作
     [self zf_playerDownload:wmplayer.playerModel.videoURL.absoluteString];
-//    [self zf_playerDownload:@"http://163.com-www-letv.com/20180604/1403_6de54f23/index.m3u8"];
-//    [self zf_playerDownload:@"http://10.2.12.0/movie/SNIS-896.mp4"];
 }
 
--(void)wmplayerFinishedPlay:(WMPlayer *)wmplayer{
+- (void)wmplayerFinishedPlay:(WMPlayer *)wmplayer {
     if (![self.navigationController popViewControllerAnimated:YES]) {
         if (self.backCompleteBlock) {
             self.backCompleteBlock(YES);
@@ -223,6 +258,5 @@
         [self dismissViewControllerAnimated:YES completion:nil];
     }
 }
-
 
 @end
